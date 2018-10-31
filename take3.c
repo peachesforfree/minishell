dbefc08199aaa2fc41baf58e249550bfd7adb9be
sergio balcorta <sergio.shearing@gmail.com>
2018-10-30 20:45:38 -1000

    made changes in take2.c and starting anew in take3.c

---
+++ take3.c
@@ -0,0 +1,18 @@
+#include "/libft/libft.h"
+
+typedef struct      s_env
+{
+    char            **user_in;
+    char            **environ;
+    char            *buffer;
+    int             buffer_size;
+}                   t_env;
+
+
+
+int         main(int argc, char **argv, char **environ)
+{
+
+
+
+}
--- test.c
+++ test.c
@@ -7,23 +7,30 @@
 #include <fcntl.h>
 #include <unistd.h>
 #include "libft/libft.h"
-
+#include <sys/types.h>
 
 
 
 int main(int argc, char **argv, char **environ)
 {
-    int i;
-    char **ret;
+    char *input;
+    char    *ret;
+    int     chr;
 
-    i = 0;
-    ret = parse(argv[1]);
+    input = NULL;
+    ret = NULL;
+    chr = 0;
+    
+    ret = getcwd(NULL, 0);
+    while (get_next_line(1, &input))
+    {
 
-//    while (ret[i] != NULL)
-//    {
-//        printf("%s\n", ret[i]);
-//        i++;
-//    }
+        chr = chdir(input);
+        if (chr == 0)
+            printf("success\t %s/%s \t is a proper directory\n", ret, input );
+        else
+            printf("Sorry %s/%s is not a proper directory\n", ret, input);
+    }
 
     return (0);
 }
