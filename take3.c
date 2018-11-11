000c04d6d356205a373594556790c196f36c37a6
Sergio Balcorta <sergio.shearing@gmail.com>
2018-11-11 14:01:29 -0800

    working on setenv built in function line 634

--- take3.c
+++ take3.c
@@ -471,14 +471,40 @@ int     ft_cd(t_env *env, char **argv_ptr)
     return (ret);
 }
 
-int     in_list(char *str, t_list *list)
+/*
+** int  ft_strccmp(char *s1, char *s2, char c)
+**will compare each char in the string until either differes
+**only when the s2 is qual to special char AND s1 is '\0' meaning both strings compare upto the special char
+*/
+
+int     ft_strccmp(char *s1, char *s2, char c)
 {
-    //make a string compare here
-    //if the string exists in any of the list->content strings before the '=' then return 1
-    // search through the program and look if i have already made something like this. (i think i have)
+    int     i;
 
-
+    i = 0;
+    while (s1[i] != '\0' && s2[i] != '\0')
+    {
+        if (s1[i] != s2[i])
+            break;
+        i++;
+    }
+    if (s2[i] == c && s1[i] == '\0')
+            return (1);
     return (0);
+}
+
+t_list     *in_list(char *str, t_list *list)
+{
+    char    *env_name;
+
+    while (list != NULL)
+    {
+        if (ft_strccmp(str, list->content, '='))
+            return (list);
+        list = list->next;
+    }
+    // search through the program and look if i have already made something like this. (i think i have)
+    return (NULL);
 }
 
 int     ft_setenv(t_env *env, char **argv_ptr)
@@ -486,13 +512,15 @@ int     ft_setenv(t_env *env, char **argv_ptr)
     int     ret;
     int     argc;
     char    *temp;
+    t_list  *list;
 
     argc = 0;
     (void)env;
     while (argv_ptr[argc] != NULL)
         argc++;
     ret = 0;
-    if (argc >= 2 && argc <= 3 && !(in_list(argv_ptr[1], env->environ)))
+    list = in_list(argv_ptr[1], env->environ);
+    if (argc >= 2 && argc <= 3 && list == NULL
     {
         temp = ft_strdup(argv_ptr[1]);
         temp = ft_strnjoin( temp, "=",1);
@@ -500,6 +528,12 @@ int     ft_setenv(t_env *env, char **argv_ptr)
             temp = ft_strnjoin(temp, argv_ptr[2], 1);
         ft_lstappend(env->environ, temp, ft_strlen(temp));
         free(temp);
+    }
+    else if (list != NULL)
+    {
+        //make use of the list element and modify values within it
+        //if no more arguments ... remove all after '=' char
+        //if THERE ARE ARGUMENTS ... change the stuff after '=' to what is in arguments
     }
     else
     {
@@ -637,8 +671,8 @@ int         main(int argc, char **argv, char **environ)
  * 
  *                   
  *                  
- *                             Line 474 - finish off the function
- *                              int     in_list(char *str, t_list *list)
+ *                             Line 634
+ *                              
 
  * 
  * 
